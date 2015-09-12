ac60428d10b2ff903e2a309c9df85c1f8e1cbce3
zhentian-wan <zhentian@monkeyexperience.com>
2015-09-12 17:05:46 +0300

    fix error

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
-var favicon = require('server-favicon');
+var favicon = require('serve-favicon');
 
 app.get('/', function (req,res) {
     res.send('Hello World!');
 });
+
 app.use(favicon(__dirname + '/public/favicon.ico'));
-app.listen(80, function () {
+
+app.listen(3112, function () {
     console.log("App is listen at port 3112" );
 });
