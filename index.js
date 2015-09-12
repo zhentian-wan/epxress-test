c2f2c4aa1dbadbc49aad36b76a9534a4a5dc41c3
zhentian-wan <zhentian@monkeyexperience.com>
2015-09-12 17:16:26 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var favicon = require('serve-favicon');
 
-app.use(favicon(__dirname + '/public/favicon.ico'))
+app.use(favicon(__dirname + '/favicon.ico'));
 
 app.get('/', function (req, res) {
     res.send('Hello world')
