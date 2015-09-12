b967af80e4ee056eefbd9dfdfac8763c957feed5
zhentian-wan <zhentian@monkeyexperience.com>
2015-09-12 16:49:50 +0300

    swithc to 80 prot

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req,res) {
     res.send('Hello World!');
 });
 
-app.listen(3112, function () {
+app.listen(80, function () {
     console.log("App is listen at port 3112" );
 });
