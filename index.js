4da2cba8dfbd4764091362aa1f777bf567fef108
zhentian-wan <zhentian@monkeyexperience.com>
2015-09-12 16:53:19 +0300

    add favicon

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+var favicon = require('server-favicon');
 
 app.get('/', function (req,res) {
     res.send('Hello World!');
 });
-
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.listen(80, function () {
     console.log("App is listen at port 3112" );
 });
